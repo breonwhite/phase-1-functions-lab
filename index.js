7f81a8e368ee44fe8eb91d94bf46428c3ef3b0ef
Breon White <bdwhite92@gmail.com>
2021-11-29 17:21:01 -0500

    commit tree 2a173750a4a5406e5b3a2243fe253ea821c60987

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+const hqLocation = 42;
+
+function distanceFromHqInBlocks(location) {
+    let distanceB = location - hqLocation;
+    return Math.abs(distanceB);
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet >= 2500) {
+        return `cannot travel that far`
+    } else if (feet >= 2000) {
+        return 25;
+    } else if (feet >= 400) {
+        return (feet-400)*0.02;
+    } else if (feet < 400) {
+        return 0;
+    }
+}
